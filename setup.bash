d4ff6fd4823f9310d883b7a38025a1c52fe7ccb0
Peppa Root <root@peppa>
2014-08-24 23:24:31 +0000

    Install django and set up db.

--- setup.bash
+++ setup.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-apt-get install quantlib-python nginx python-flup
+#useradd pi?
+
+apt-get install quantlib-python nginx python-django
 
 service nginx start
 
+cd /home/pi/pfp
+django-admin startproject pyprice
+cd pyprice 
+./manage.py syncdb
