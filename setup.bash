127ad620f80d506aa861c0cb3a043c5dbd4ddf05
Peppa Root <root@peppa>
2014-08-24 23:50:50 +0000

    Turn on the dev server built-in to Django.

--- setup.bash
+++ setup.bash
@@ -9,4 +9,9 @@ service nginx start
 cd /home/pi/pfp
 django-admin startproject pyprice
 cd pyprice 
+# Do the changes to settings first
 ./manage.py syncdb
+# This is only for testing - remove it when we have details of
+# how to setup the prod server
+./manage.py runserver 192.168.0.6:8000
+
