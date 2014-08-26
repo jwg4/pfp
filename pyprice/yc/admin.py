fefb917dbe80bb1e9574dee51b88ce486d6d6621
Jack Grahl <pi@peppa>
2014-08-26 14:32:46 +0000

    Add some navigation widgets to YC listing.

--- pyprice/yc/admin.py
+++ pyprice/yc/admin.py
@@ -13,7 +13,11 @@ class YCAdmin(admin.ModelAdmin):
     fields = ['name', 'currency', 'pricing_date']
     inlines = [CashInline, SwapInline]
     list_display = ['name','currency', 'pricing_date']
-    
+    list_filter = ['currency']
+    list_filter = ['pricing_date']
+    search_fields = ['question']
+    date_hierarchy = ['pricing_date']
+
 admin.site.register(YieldCurve, YCAdmin)
 #admin.site.register(CashRate)
 #admin.site.register(SwapRate)
