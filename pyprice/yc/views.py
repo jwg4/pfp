fdfab5b5025919c69aeae6646c5413c0e87caa2f
Jack Grahl <pi@peppa>
2014-08-26 15:26:54 +0000

    Add view to list yield curves.

--- pyprice/yc/views.py
+++ pyprice/yc/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import HttpResponse
+from yc.models import YieldCurve
+from django.template import Context, loader
+from django.shortcuts import render_to_response
+
+def index(request):
+    curves = YieldCurve.objects.all().order_by('-pricing_date')
+    return render_to_response('yc/index.html', {'curves': curves})
+    #t = loader.get_template('yc/index.html')
+    #c = Context({
+    #        'curves': curves,
+    #        })
+    #return HttpResponse(t.render(c))
