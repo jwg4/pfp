f9d1e351bf32d019229227d2828fee5f5e00172e
Peppa Root <root@peppa>
2014-08-26 08:18:24 +0000

    Changes to /etc to setup uwsgi - new setup.bash.
    We now install uwsgi and uwsgi-plugin-python, and the configs for all this are in /etc/uwsgi and /etc/nginx

--- setup.bash
+++ setup.bash
@@ -2,7 +2,7 @@
 
 #useradd pi?
 
-apt-get install quantlib-python nginx python-django
+apt-get install quantlib-python nginx python-django uwsgi uwsgi-plugin-python
 
 service nginx start
 
@@ -11,7 +11,5 @@ django-admin startproject pyprice
 cd pyprice 
 # Do the changes to settings first
 ./manage.py syncdb
-# This is only for testing - remove it when we have details of
-# how to setup the prod server
-./manage.py runserver 192.168.0.6:8000
 
+service uwsgi start
