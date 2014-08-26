14914aa37e651951ec4278c4c2215144b738d4a6
Jack Grahl <pi@peppa>
2014-08-26 10:53:10 +0000

    Add __unicode__ representation to yc objects.

--- pyprice/yc/models.py
+++ pyprice/yc/models.py
@@ -4,8 +4,12 @@ from QuantLib import *
 # Create your models here.
 
 class YieldCurve(models.Model):
+    name = models.CharField(max_length=30)
     pricing_date = models.DateTimeField()
     currency = models.CharField(max_length=3)
+
+    def __unicode__(self):
+        return "%s %s" % (name, currency)
 
 class Pillar(models.Model):
     yield_curve = models.ForeignKey(YieldCurve)
@@ -13,6 +17,12 @@ class Pillar(models.Model):
 
 class CashRate(Pillar):
     months = models.IntegerField()
+
+    def __unicode__(self):
+        return "%s %dM CASH" % (yield_curve.currency, months)
     
 class SwapRate(Pillar):
     years = models.IntegerField()
+
+    def __unicode__(self):
+        return "%s %dY SWAP" % (yield_curve.currency, years)
