ac11cbcf8b8786fb0485911e5a04f75a86da07d2
Jack Grahl <pi@peppa>
2014-08-27 13:03:34 +0000

    Remove DEBUG settings and add to list of hosts.

--- pyprice/pyprice/settings.py
+++ pyprice/pyprice/settings.py
@@ -1,6 +1,6 @@
 # Django settings for pyprice project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -22,7 +22,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["peppa", "127.0.0.1", "192.168.0.6"]
+ALLOWED_HOSTS = ["peppa", "127.0.0.1", "192.168.0.6", "82.12.247.232"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
